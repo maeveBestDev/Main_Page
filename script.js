a07aa26963b05530b474bfaf628bffd98390cebb
maeveBestDev <77807261+maeveBestDev@users.noreply.github.com>
2021-03-09 13:33:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById("input").onsearch = function() {controle()};
 function controle(){
 var saisie = document.getElementById("input");
 if (saisie == "anime"){
-  document.location.href="https://nsi-team.github.io/Anime/";
+  document.location.href="https://maevebestdev.github.io/GIF_Mignon/%C2%BB/search?%C2%BBq=anime";
   }
 }
